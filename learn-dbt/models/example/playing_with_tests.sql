bde5255db9ca95e2abb53d55ad352bfe0278199e
Jeremy Holtzman <jeremyholtzman@gmail.com>
2020-04-13 17:15:28 -0400

    bug fix playing with tests

--- learn-dbt/models/example/playing_with_tests.sql
+++ learn-dbt/models/example/playing_with_tests.sql
@@ -1,10 +1,11 @@
 with sample_customer as (
   SELECT *
-  FROM {{ source('sample', 'customer') }}
+  FROM {{ source('sample2', 'customer') }}
 )
 
 select
   c_custkey,
   c_mktsegment,
-  {{rename_segments('c_mktsegment')}} mkt_segment_adjusted
+  {{rename_segments('c_mktsegment')}} mkt_segment_adjusted,
+  c_acctbal
 from sample_customer
