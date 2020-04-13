d0697351abe6bc2bf0487a5d670bc7c2bf75e863
Jeremy Holtzman <jeremyholtzman@gmail.com>
2020-04-13 10:27:29 -0400

    WIP

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -10,11 +10,13 @@
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, 'NY' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
     union all
-    select null as id
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
     union all
-    select 3 as id
+    select 4 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    union all
+    select 5 as id, 'NJ' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 
 )
 
--- learn-dbt/models/example/snowflake_customer_purchases.sql
+++ learn-dbt/models/example/snowflake_customer_purchases.sql
@@ -3,8 +3,8 @@ SELECT
     c.c_name,
     c.c_nationkey as nation,
     sum(o.o_totalprice) as total_order_price
-from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
-LEFT JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+from {{ source('sample', 'customer') }} c
+LEFT JOIN {{ source('sample', 'orders') }} o
 ON c.c_custkey = o.o_custkey
 
 group by
